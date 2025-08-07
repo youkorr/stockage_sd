20f4f8cf21207adad0baf5a3fb41cbc3d4ee4b26
youkorr <96953496+youkorr@users.noreply.github.com>
2025-08-07 18:19:02 +0200

    Update image.h

--- components/image/image.h
+++ components/image/image.h
@@ -1,7 +1,10 @@
 #pragma once
 #include "esphome/core/color.h"
 #include "esphome/components/display/display.h"
+
+#ifdef USE_SD_MMC_CARD
 #include "../sd_mmc_card/sd_mmc_card.h"
+#endif
 
 #ifdef USE_LVGL
 #include "esphome/components/lvgl/lvgl_proxy.h"
@@ -17,7 +20,7 @@ enum ImageType {
   IMAGE_TYPE_RGB565 = 3,
 };
 
-enum Transparency {
+enum TransparencyType {  // Renommé pour correspondre au Python
   TRANSPARENCY_OPAQUE = 0,
   TRANSPARENCY_CHROMA_KEY = 1,
   TRANSPARENCY_ALPHA_CHANNEL = 2,
@@ -25,7 +28,7 @@ enum Transparency {
 
 class Image : public display::BaseImage {
  public:
-  Image(const uint8_t *data_start, int width, int height, ImageType type, Transparency transparency);
+  Image(const uint8_t *data_start, int width, int height, ImageType type, TransparencyType transparency);
   Color get_pixel(int x, int y, Color color_on = display::COLOR_ON, Color color_off = display::COLOR_OFF) const;
   int get_width() const override;
   int get_height() const override;
@@ -44,6 +47,7 @@ class Image : public display::BaseImage {
 #ifdef USE_LVGL
   lv_img_dsc_t *get_lv_img_dsc();
 #endif
+
  protected:
   bool get_binary_pixel_(int x, int y) const;
   Color get_rgb_pixel_(int x, int y) const;
@@ -54,13 +58,52 @@ class Image : public display::BaseImage {
   int height_;
   ImageType type_;
   const uint8_t *data_start_;
-  Transparency transparency_;
+  TransparencyType transparency_;
   size_t bpp_{};
   size_t stride_{};
 #ifdef USE_LVGL
   lv_img_dsc_t dsc_{};
 #endif
 };
+
+#ifdef USE_SD_MMC_CARD
+class SDCardImage : public Image {
+ public:
+  SDCardImage(const std::string &path, ImageType type, TransparencyType transparency);
+  ~SDCardImage();
+
+  // Configuration methods (appelées depuis Python)
+  void set_resize(int width, int height) { 
+    this->resize_width_ = width; 
+    this->resize_height_ = height; 
+  }
+  void set_dither(bool dither) { this->dither_ = dither; }
+  void set_invert_alpha(bool invert) { this->invert_alpha_ = invert; }
+  void set_big_endian(bool big_endian) { this->big_endian_ = big_endian; }
+
+  // Override des méthodes de base
+  void draw(int x, int y, display::Display *display, Color color_on, Color color_off) override;
+  Color get_pixel(int x, int y, Color color_on = display::COLOR_ON, Color color_off = display::COLOR_OFF) const override;
+
+ protected:
+  bool load_image_();
+  void process_image_data_();
+  void cleanup_();
+
+  std::string path_;
+  uint8_t *image_data_;
+  bool loaded_;
+  
+  // Options de configuration
+  int resize_width_{0};
+  int resize_height_{0};
+  bool dither_{false};
+  bool invert_alpha_{false};
+  bool big_endian_{true};
+  
+  sd_mmc_card::SDMMCCardComponent *sd_card_{nullptr};
+};
+#endif
 
 }  // namespace image
 }  // namespace esphome
